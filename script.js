3bd2c6badc276a1db01fe28315c7a947aa429335
Pavitrank <pavitrank2002@gmail.com>
2023-12-20 10:06:21 +0530

    Final Page done

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-console.log("Shopify website");
+// console.log("Shopify website");
 
-let iconBag =  document.querySelector('.bag');
-let body = document.querySelector('body');
-let closeCart = document.querySelector('.close');
+// let iconBag =  document.querySelector('.bag');
+// let body = document.querySelector('body');
+// let closeCart = document.querySelector('.close');
 
-iconBag.addEventListener('click', () => {
-    console.log("Toggle show cart");
-    body.classList.toggle('showCart');
-})
-closeCart.addEventListener('click', () => {
-    console.log("Showcart close");
-    body.classList.toggle('showCart');
-})
+// iconBag.addEventListener('click', () => {
+//     console.log("Toggle show cart");
+//     body.classList.toggle('showCart');
+// })
+// closeCart.addEventListener('click', () => {
+//     console.log("Showcart close");
+//     body.classList.toggle('showCart');
+// })
