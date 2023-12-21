ebf5dcc432097cbfc4cd3407c52133f14f5bc83b
Pavitrank <pavitrank2002@gmail.com>
2023-12-21 22:46:30 +0530

    Items rendered

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ const productEl = document.querySelector(".container");
 // RENDER ELEMENTS
 function renderProducts() {
   products.forEach((product) => {
+    if (product.id >= 9) {
+      return;
+    }
     productEl.innerHTML += `
         <div class="pro-container">
         <img src="${product.imgSrc}" alt="" />
@@ -47,10 +50,46 @@ function renderProducts() {
         </div>
       </div>
       `;
-    console.log(product);
   });
 }
 
 renderProducts();
 
-// console.log(product);
+const productEl2 = document.querySelector("#new-container");
+
+function renderExtraProducts() {
+  products.forEach((product) => {
+    if (product.id <= 8) {
+      return;
+    }
+
+    productEl2.innerHTML += `<div class="pro-container">
+    <img src="${product.imgSrc}" alt="" />
+    <div class="lower">
+      <div class="des">
+        <span> ${product.name}</span>
+        <p>Cartoon Astronaut T-Shirts</p>
+        <div class="star">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p class="price">$ ${product.price}</p>
+      </div>
+      <div class="cart">
+        <a href="cart.html">
+          <img
+            src="img/shopping_cart_FILL0_wght400_GRAD0_opsz24.png"
+            alt=""
+          />
+        </a>
+      </div>
+    </div>
+  </div>`;
+    console.log(product.id);
+  });
+}
+
+renderExtraProducts();
